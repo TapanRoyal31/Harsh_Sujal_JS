fb9d84d703b584fa8c05a283be60182010b42321
TapanRoyal31 <tapanr3105@gmail.com>
2026-01-24 16:55:34 +0530

    "other function"

---
+++ 07_Functions/OtherFunction.js
@@ -0,0 +1,26 @@
+// IIFE :- Imidiate Invoke Function Expression
+
+// (function () {
+//   console.log("IIFE Called!!");
+// }) ();
+
+// Default parameter function
+// function greet(name = "guest"){
+//     console.log("greet function called",name);
+// }
+
+// greet();
+
+
+// Nested Function
+// function outer(surname){
+//     function inner(lastname){
+//         return surname + lastname;
+//     }
+//     return inner;
+// }
+
+
+// let result = outer("Thakkar ");
+// console.log(result("Harsh"));
+
