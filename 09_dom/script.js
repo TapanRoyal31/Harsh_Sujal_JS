94553546e1983a5730bda61748465ab769dc04eb
TapanRoyal31 <tapanr3105@gmail.com>
2026-01-30 18:46:13 +0530

    "dom basics"

---
+++ 09_dom/script.js
@@ -0,0 +1,19 @@
+// dom :- document object model
+
+// document :- store html code as object
+
+/*
+    getElementById - return only first element
+    getElementbyClassName - return html collection
+    queryselector - return only first element (use css selectors)
+    querySelectorALL - return nodelist 
+*/
+
+// let par = document.getElementById('par');
+// console.log(par);
+
+// let par1 = document.getElementsByClassName('para');
+// console.log(par1);
+
+// let par = document.querySelectorAll('.para');
+// console.log(par);
