58f3bc881f6ad495b79af089f13779c446b0e740
TapanRoyal31 <tapanr3105@gmail.com>
2026-02-07 17:23:50 +0530

    "queryselector and queryselectorall"

--- 09_dom/script.js
+++ 09_dom/script.js
@@ -1,7 +1,10 @@
-// 1) getelementbyId
-// 2) getelementbyclassname
-// 3) queryselector
-// 4) queryselectorAll
+// 1) getElementById :- select single element using id name from html tag
+// 2) getElementsByClassName :- select list of element using class name from html 
+//                             it can be one or more html tags based on same class name
+// 3) querySelector :- select first element using css selector 
+// 4) querySelectorAll :- select list node using css selector based on same css selector values 
+//                             it can be one or more node element based on css selector values
+
 
 
 // --------------------------------------------------------------------------
@@ -24,3 +27,26 @@
 //     const element = class1[i];
 //     element.style.background = 'yellow';
 // }
+
+// class1[1].style.background = 'red'
+
+
+//-----------------------------------------------------------------------------
+
+// let query = document.querySelector('p');
+// console.log(query);
+
+// query.style.background = 'red';
+
+// ----------------------------------------------------------------------------
+
+// let queryAll = document.querySelectorAll('.paragraph');
+
+
+// console.log(queryAll);
+
+// for (let i = 0; i < queryAll.length; i++) {
+//     const element = queryAll[i];
+//     element.style.background = 'blue';
+// }
+
