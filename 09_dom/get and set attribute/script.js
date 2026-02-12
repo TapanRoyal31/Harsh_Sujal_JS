63b288a056320f93b23b5fa62f6f62635ae02db1
TapanRoyal31 <tapanr3105@gmail.com>
2026-02-12 18:55:57 +0530

    "get and set , add new element"

---
+++ 09_dom/get and set attribute/script.js
@@ -0,0 +1,20 @@
+// let par = document.querySelector('.a');
+
+// console.log(par);
+//-----------------------------------------------------------------------------------------------------
+// get and set attribute :-
+
+// console.log(par.getAttribute('title')); // get attribute of element
+// console.log(par.setAttribute('style','color:red')); // to set attribute of element
+
+// -----------------------------------------------------------------------------------------------------
+// add new element:- 
+
+// let par = document.querySelector('p');
+// let newEle = document.createElement('h1');
+
+// par.appendChild(newEle);
+
+
+// -----------------------------------------------------------------------------------------------------
+
